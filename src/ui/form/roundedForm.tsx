ffbde4d735d633abaa50e27642ea525a7994d25b
Maday Concepcion <yoet92@gmail.com>
2022-07-19 11:59:10 -0300

    add rounded form

---
+++ src/ui/form/roundedForm.tsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+const StyledForm = styled.form`
+  display: grid;
+  gap: 4px;
+
+  &:first-child:not(style) {
+    border-radius: 1rem 0 0 1rem;
+  }
+
+  &:last-child:not(style) {
+    border-radius: 0 1rem 1rem 0;
+  }
+`;
+
+interface FormProps extends React.PropsWithChildren {
+  onSubmit: React.FormEventHandler,
+}
+
+export const RoundedForm = ({ children, onSubmit }: FormProps): JSX.Element => {
+  return (
+    <StyledForm onSubmit={onSubmit}>{children}</StyledForm>
+  );
+};
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,3 +1,4 @@
 export * from './layout/layoutComponent';
 export * from './nav/navComponent';
 export * from './nav/navMenuItem';
+export * from './form/roundedForm';
